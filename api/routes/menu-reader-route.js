d4505790555ce033747302c8c9e377c2ba2c8626
Rahul Mahanty <33182277+mahantyr@users.noreply.github.com>
2021-02-21 22:41:07 +0530

    Update menu-reader-route.js

--- api/routes/menu-reader-route.js
+++ api/routes/menu-reader-route.js
@@ -2,16 +2,31 @@
 module.exports = function(app) {
   var todoList = require('./menu-reader');
 
-app.route('/Eatery/:restName')
-.get(todoList.get_menu_type);
+app.route('/getEateryID/:restName')
+.get(todoList.get_eatery_id); 
 
-app.route('/Eatery/:restName/:type')
-.get(todoList.get_item_by_type);
+app.route('/Eatery/:restNameID')
+.get(todoList.get_menu_type); 
 
-app.route('/Eatery/:restName/:type')
-.post(todoList.add_item_by_type);
+app.route('/Eatery/:restNameID')
+.post(todoList.add_menu_type); 
 
-app.route('/Eatery/:restName/:type')
-.delete(todoList.delete_item_in_type);
+app.route('/Eatery/:restNameID/:type')
+.put(todoList.update_menu_type); 
+
+app.route('/Eatery/:restNameID/:type')
+.delete(todoList.delete_menu_type); 
+
+app.route('/Eatery/:restNameID/:type')
+.get(todoList.get_item_by_type); 
+
+app.route('/Eatery/:restNameID/:type')
+.post(todoList.add_item_by_type); 
+
+app.route('/Eatery/:restNameID/:type/:itemID')
+.delete(todoList.delete_item_by_type); 
+
+app.route('/Eatery/:restNameID/:type/:itemID')
+.put(todoList.update_item_by_type);
 
 };
