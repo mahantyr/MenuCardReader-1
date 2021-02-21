a576ce14aaca90052959e7509322dcb09f38d509
Rahul Mahanty <33182277+mahantyr@users.noreply.github.com>
2021-02-21 22:42:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/menu-reader-route'); //importing route
 routes(app); //register the route
 
+app.use(function (req,res,next){
+	res.status(404).send('404 Error: Unable to find the requested resource in MCR API');
+});
+
 app.listen(port);
 
 console.log('Menucardreader RESTful API server started on: ' + port);
